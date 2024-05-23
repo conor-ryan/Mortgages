237fa70edc2cd60125c3c5a741510f23743480e7
conor-ryan <conor.ryan21@gmail.com>
2024-05-23 17:08:34 -0400

    Use the strict alpha threshold

--- EquilibriumFunctions.py
+++ EquilibriumFunctions.py
@@ -412,14 +412,14 @@ def solve_eq_r_optim(r0,j,d,theta,m,model="base",return_bound=False):
     alpha, r, itr = solve_eq_r(r0,j,d,theta,m,itr_max=itr_max,model=model)
     if itr<itr_max:
         if alpha < theta.alpha_min:
-            r_min = ModelFunctions.min_rate(d,theta,m,model=model)
-            if model=="base": 
-                prof, dprof = ModelFunctions.dSaleProfit_dr(np.repeat(r0,len(r_min)),d,theta,m)
-            elif model=="hold":
-                prof, dprof = ModelFunctions.dHoldOnly_dr(np.repeat(r0,len(r_min)),d,theta)
-            alpha_max = -dprof[j]/prof[j] 
-            alpha = alpha_max
-            r = r_min - 1/alpha_max
+            # r_min = ModelFunctions.min_rate(d,theta,m,model=model)
+            # if model=="base": 
+            #     prof, dprof = ModelFunctions.dSaleProfit_dr(np.repeat(r0,len(r_min)),d,theta,m)
+            # elif model=="hold":
+            #     prof, dprof = ModelFunctions.dHoldOnly_dr(np.repeat(r0,len(r_min)),d,theta)
+            # alpha_max = -dprof[j]/prof[j] 
+            # alpha = theta.alpha_min
+            r = ModelFunctions.min_rate(d,theta,m,model=model) - 1/theta.alpha_min
             alpha_bound = 1
         if return_bound:
             return alpha, r, itr, True, alpha_bound
@@ -488,8 +488,8 @@ def solve_eq_r_optim(r0,j,d,theta,m,model="base",return_bound=False):
 
 
     if alpha < theta.alpha_min:
-        alpha = alpha_max
-        r = ModelFunctions.min_rate(d,theta,m,model=model) - 1/alpha_max
+        alpha = theta.alpha_min
+        r = ModelFunctions.min_rate(d,theta,m,model=model) - 1/theta.alpha_min
     if return_bound:
         return alpha,r, res.nfev, res.success, alpha_bound
     else:    

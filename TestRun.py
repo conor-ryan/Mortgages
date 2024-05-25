b79df8eebe3cd819dcb417b59ce945bd9d953cc6
Conor Ryan <ryan0463@umn.edu>
2024-05-25 19:58:13 -0400

    test

--- TestRun.py
+++ TestRun.py
@@ -150,11 +150,11 @@ NUM_WORKERS = 16
 start_parameters = np.zeros(len(true_parameters))
 
 
-print("Estimate in Parallel")
-f_val, res = estimate_NR(start_parameters,theta,consumer_data,market_data,mbs_data,parallel=True,num_workers=NUM_WORKERS,gtol=1e-8)
+# print("Estimate in Parallel")
+# f_val, res = estimate_NR(start_parameters,theta,consumer_data,market_data,mbs_data,parallel=True,num_workers=NUM_WORKERS,gtol=1e-8)
 
-# print("Estimate in Parallel with precondition")
-# f_val, res = estimate_NR(start_parameters,theta,consumer_data,market_data,mbs_data,parallel=True,num_workers=NUM_WORKERS,gtol=1e-4,pre_condition=True)
+print("Estimate in Parallel with precondition")
+f_val, res = estimate_NR(start_parameters,theta,consumer_data,market_data,mbs_data,parallel=True,num_workers=NUM_WORKERS,gtol=1e-8,pre_condition=True)
 
 
 # print("Estimate without parallel")

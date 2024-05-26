f13a98faf7a4761aa2c75a7cd75b93533e3a4b6d
Conor Ryan <ryan0463@umn.edu>
2024-05-26 14:46:47 -0400

    Attempt lower tolerance

--- TestRun.py
+++ TestRun.py
@@ -154,7 +154,7 @@ start_parameters = np.zeros(len(true_parameters))
 # f_val, res = estimate_NR(start_parameters,theta,consumer_data,market_data,mbs_data,parallel=True,num_workers=NUM_WORKERS,gtol=1e-8)
 
 print("Estimate in Parallel with precondition")
-f_val, res = estimate_NR(start_parameters,theta,consumer_data,market_data,mbs_data,parallel=True,num_workers=NUM_WORKERS,gtol=1e-6,pre_condition=True)
+f_val, res = estimate_NR(start_parameters,theta,consumer_data,market_data,mbs_data,parallel=True,num_workers=NUM_WORKERS,gtol=1e-8,pre_condition=True)
 
 
 # print("Estimate without parallel")

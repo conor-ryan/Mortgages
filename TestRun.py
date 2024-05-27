ff2d81901fef2e8cf178acd84f3b80ca7a5831c2
Conor Ryan <ryan0463@umn.edu>
2024-05-27 18:44:52 -0400

    real cost est

--- TestRun.py
+++ TestRun.py
@@ -96,7 +96,7 @@ theta = Parameters(consumer_data,
 
 cost_true = np.array([-0.01,-0.005,0.002,#Gamma_WH
                       0.4,-1.4e-4,-3.5e-5,0,0,0.00,0,0.3])
-theta.set_cost(cost_true)
+theta.set_cost(cost_res.x)
 true_parameters = np.array([9.3,9.1, 8.9, 8.7,8.5,30.0])#, # Beta_x
                 #    0,0,0, #Gamma_WH
                 #    0.32,0]) # Gamma_ZH

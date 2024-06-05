0872bdb9f8011c2c202ebacd37fa25ab9b574abd
Conor Ryan <ryan0463@umn.edu>
2024-06-05 11:09:24 -0400

    test with lower alpha

--- TestRun.py
+++ TestRun.py
@@ -92,48 +92,48 @@ true_parameters = np.array([9.3,9.1, 8.9, 8.7,8.5,0])#, # Beta_x
                 #    0,0,0, #Gamma_WH
                 #    0.32,0]) # Gamma_ZH
 
-### Run Timing Tests ####
-clist = consumer_object_list(theta,consumer_data,market_data,mbs_data)
-print("Timing 2 Cores")
-for i in range(5):
-    start = time.perf_counter()
-    res =  evaluate_likelihood_hessian(true_parameters,theta,clist,
-                                       parallel=True,num_workers=2)
-    end = time.perf_counter()
-    elapsed = end - start
-    print(f'Elapsed Time: {elapsed:.6f} seconds')
+# ### Run Timing Tests ####
+# clist = consumer_object_list(theta,consumer_data,market_data,mbs_data)
+# print("Timing 2 Cores")
+# for i in range(5):
+#     start = time.perf_counter()
+#     res =  evaluate_likelihood_hessian(true_parameters,theta,clist,
+#                                        parallel=True,num_workers=2)
+#     end = time.perf_counter()
+#     elapsed = end - start
+#     print(f'Elapsed Time: {elapsed:.6f} seconds')
 
-print("Timing 4 Cores")
-for i in range(5):
-    start = time.perf_counter()
-    res =  evaluate_likelihood_hessian(true_parameters,theta,clist,
-                                       parallel=True,num_workers=4)
-    end = time.perf_counter()
-    elapsed = end - start
-    print(f'Elapsed Time: {elapsed:.6f} seconds')
-
-
-print("Timing 8 Cores")
-for i in range(5):
-    start = time.perf_counter()
-    res =  evaluate_likelihood_hessian(true_parameters,theta,clist,
-                                       parallel=True,num_workers=8)
-    end = time.perf_counter()
-    elapsed = end - start
-    print(f'Elapsed Time: {elapsed:.6f} seconds')
+# print("Timing 4 Cores")
+# for i in range(5):
+#     start = time.perf_counter()
+#     res =  evaluate_likelihood_hessian(true_parameters,theta,clist,
+#                                        parallel=True,num_workers=4)
+#     end = time.perf_counter()
+#     elapsed = end - start
+#     print(f'Elapsed Time: {elapsed:.6f} seconds')
 
 
-print("Timing 16 Cores")
-for i in range(5):
-    start = time.perf_counter()
-    res =  evaluate_likelihood_hessian(true_parameters,theta,clist,
-                                       parallel=True,num_workers=16)
-    end = time.perf_counter()
-    elapsed = end - start
-    print(f'Elapsed Time: {elapsed:.6f} seconds')
+# print("Timing 8 Cores")
+# for i in range(5):
+#     start = time.perf_counter()
+#     res =  evaluate_likelihood_hessian(true_parameters,theta,clist,
+#                                        parallel=True,num_workers=8)
+#     end = time.perf_counter()
+#     elapsed = end - start
+#     print(f'Elapsed Time: {elapsed:.6f} seconds')
 
-NUM_WORKERS = 16
-start_parameters = np.zeros(len(true_parameters))
+
+# print("Timing 16 Cores")
+# for i in range(5):
+#     start = time.perf_counter()
+#     res =  evaluate_likelihood_hessian(true_parameters,theta,clist,
+#                                        parallel=True,num_workers=16)
+#     end = time.perf_counter()
+#     elapsed = end - start
+#     print(f'Elapsed Time: {elapsed:.6f} seconds')
+
+# NUM_WORKERS = 16
+# start_parameters = np.zeros(len(true_parameters))
 
 
 # print("Estimate in Parallel")

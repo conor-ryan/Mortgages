215d2576360c0fc66f886030a51f9f8951975b94
Conor Ryan <ryan0463@umn.edu>
2024-06-10 15:05:39 -0400

    another way to include skipped q0

--- KernelFunctions.py
+++ KernelFunctions.py
@@ -51,7 +51,8 @@ def macro_likelihood(a_list,c_list_H,c_list_S,q0_list,theta,skip_vec):
         skip_share = sum(skip_mkt)/len(skip_mkt)
 
         # pred_out[o] = outside_share(a_mkt,c_mkt_H,c_mkt_S,q0_mkt,theta.out_share[o])
-        pred_out[o] = np.mean(q0_mkt[skip_mkt==False])
+        pred_out[o] = np.mean(q0_mkt[skip_mkt==False])*(1-skip_share) +  \
+        np.mean(q0_mkt[skip_mkt==True])*(skip_share)
     ll_macro = np.sum(theta.N*theta.out_share*np.log(pred_out)) + \
                     np.sum(theta.N*(1-theta.out_share)*np.log(1-pred_out))
     return ll_macro
@@ -72,11 +73,14 @@ def macro_likelihood_grad(a_list,c_list_H,c_list_S,q0_list,
         dq0_mkt = dq0_list[ind]#[theta.out_sample[o]]
 
         skip_mkt = skip_vec[ind]
+        skip_share = sum(skip_mkt)/len(skip_mkt)
 
         # out, g= out_share_gradient(a_mkt,c_mkt_H,c_mkt_S,q0_mkt,
         #                            da_mkt,dq0_mkt,theta.out_share[o],theta)
-        out = np.mean(q0_mkt[skip_mkt==False])
-        g = np.mean(dq0_mkt[skip_mkt==False,:],0)
+        out = np.mean(q0_mkt[skip_mkt==False])*(1-skip_share) +  \
+        np.mean(q0_mkt[skip_mkt==True])*(skip_share)
+        g = np.mean(dq0_mkt[skip_mkt==False,:],0)*(1-skip_share)
+
         pred_out[o] = out
         x = theta.N[o]*(theta.out_share[o]*(g)/out - (1-theta.out_share[o])*(g)/(1-out) )
         grad += x
@@ -105,6 +109,7 @@ def macro_likelihood_hess(a_list,c_list_H,c_list_S,q0_list,da_list,dq0_list,d2q0
         dq0_mkt = dq0_list[ind]#[theta.out_sample[o]]
 
         skip_mkt = skip_vec[ind]
+        skip_share = sum(skip_mkt)/len(skip_mkt)
 
         d2q0_mkt = d2q0_list[ind]
         # x = theta.N[o]*theta.out_share[o]*(np.dot(grad_alpha,da_mkt) + np.dot(grad_q0,dq0_mkt))
@@ -116,15 +121,17 @@ def macro_likelihood_hess(a_list,c_list_H,c_list_S,q0_list,da_list,dq0_list,d2q0
         # out, g= out_share_gradient(a_mkt,c_mkt_H,c_mkt_S,q0_mkt,
         #                            da_mkt,dq0_mkt,theta.out_share[o],theta)
     
-        out = np.mean(q0_mkt[skip_mkt==False])
-        g = np.mean(dq0_mkt[skip_mkt==False,:],0)
+        out = np.mean(q0_mkt[skip_mkt==False])*(1-skip_share) +  \
+        np.mean(q0_mkt[skip_mkt==True])*(skip_share)
+        g = np.mean(dq0_mkt[skip_mkt==False,:],0)*(1-skip_share)
 
         pred_out[o] = out
         x = theta.N[o]*(theta.out_share[o]*(g)/out - (1-theta.out_share[o])*(g)/(1-out) )
         grad += x
 
-        y = theta.N[o]*theta.out_share[o]*(np.mean(d2q0_mkt[skip_mkt==False,:,:],0)/out - np.outer(g,g)/out**2) - \
-         theta.N[o]*(1-theta.out_share[o])*(np.mean(d2q0_mkt[skip_mkt==False,:,:],0)/(1-out) + np.outer(g,g)/(1-out)**2)
+        h = np.mean(d2q0_mkt[skip_mkt==False,:,:],0)*(1-skip_share)
+        y = theta.N[o]*theta.out_share[o]*(h/out - np.outer(g,g)/out**2) - \
+         theta.N[o]*(1-theta.out_share[o])*(h/(1-out) + np.outer(g,g)/(1-out)**2)
         hess += y
 
     ll_macro = np.sum(theta.N*theta.out_share*np.log(pred_out)) + \

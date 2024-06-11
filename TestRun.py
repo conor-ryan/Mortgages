ace97bd153ffab4b4d6f0f501203c6086c772a0f
Conor Ryan <ryan0463@umn.edu>
2024-06-10 21:52:30 -0400

    Lower tolerance

--- TestRun.py
+++ TestRun.py
@@ -154,7 +154,7 @@ start_parameters = np.zeros(len(true_parameters))
 
 print("Estimate in Parallel with precondition")
 f_val, res = estimate_NR(start_parameters,theta,consumer_data,market_data,mbs_data,parallel=True,num_workers=NUM_WORKERS,
-                         gtol=1e-6,pre_condition=True,pre_cond_itr=5,
+                         gtol=1e-8,pre_condition=True,pre_cond_itr=5,
                          max_step_size = 10)
 
 
